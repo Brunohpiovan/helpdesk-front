381f15d8ff81bd3ace4b267e7b9f3ebbec314a8d
Bruno piovan <bpiovan90@gmail.com>
2024-10-11 11:17:02 -0300

    autenticacao de login completa

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
 import { AuthService } from 'src/app/services/auth.service';
@@ -21,7 +22,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private toast:ToastrService,
-  private service: AuthService) { }
+  private service: AuthService,
+  private router: Router){ }
 
   ngOnInit(): void {
   }
@@ -29,6 +31,7 @@ export class LoginComponent implements OnInit {
   logar(){
     this.service.authenticate(this.creds).subscribe(reposta =>{
       this.service.succesfulLogin(reposta.headers.get('Authorization').substring(7));
+      this.router.navigate(['']);
     },()=>{
       this.toast.error("Usuario e/ou senha inválidos ");
     })
