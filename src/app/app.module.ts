f3303a4097e7a52ee4221c45d340cc78179a2b08
Bruno piovan <bpiovan90@gmail.com>
2024-10-14 11:24:04 -0300

    Validacao dos campos para tecnico create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthIncerceptorProvider } from './interceptors/auth.interceptor';
 import { TecnicoCreateComponent } from './components/tecnicos/tecnico-create/tecnico-create.component';
+import { NgxMaskModule } from 'ngx-mask';
 
 
 @NgModule({
@@ -76,7 +77,8 @@ import { TecnicoCreateComponent } from './components/tecnicos/tecnico-create/tec
       timeOut:4000,
       closeButton:true,
       progressBar:true
-    } )
+    } ),
+    NgxMaskModule.forRoot()
   ],
   providers: [AuthIncerceptorProvider],
   bootstrap: [AppComponent]
