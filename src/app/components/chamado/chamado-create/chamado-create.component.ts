f2a947ca4c60b9f55ea883c2b9374aa74370646e
Bruno piovan <bpiovan90@gmail.com>
2024-10-16 11:25:11 -0300

    Servico para criado chamado finalizado

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/service/chamado.service';
+import { ClienteService } from 'src/app/service/cliente.service';
+import { TecnicoService } from 'src/app/service/tecnico.service';
 
 @Component({
   selector: 'app-chamado-create',
@@ -7,6 +15,21 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./chamado-create.component.css']
 })
 export class ChamadoCreateComponent implements OnInit {
+
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+
+  clientes:Cliente[]=[]
+  tecnicos:Tecnico[]=[]
+
 
   prioridade: FormControl = new FormControl(null, [Validators.required]);
   status:     FormControl = new FormControl(null, [Validators.required]);
@@ -16,10 +39,41 @@ export class ChamadoCreateComponent implements OnInit {
   cliente:    FormControl = new FormControl(null, [Validators.required]);
 
 
-  constructor() { }
+  constructor(
+    private chamadoService: ChamadoService,
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private toastService : ToastrService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+    this.findAllCliente();
+    this.findAllTecnicos();
   }
+
+  create():void{
+    this.chamadoService.create(this.chamado).subscribe(reposta=>{
+      this.toastService.success('Chamado criado com sucesso!','Novo Chamado');
+      this.router.navigate(['chamados']);
+    },ex=>{
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+
+  findAllCliente():void{
+    this.clienteService.findAll().subscribe(reposta=>{
+      this.clientes = reposta;
+    })
+  }
+
+  findAllTecnicos():void{
+    this.tecnicoService.findAll().subscribe(reposta=>{
+      this.tecnicos = reposta;
+    })
+  }
+
 
   validaCampos():boolean{
     return this.prioridade.valid && this.status.valid && this.titulo.valid 
